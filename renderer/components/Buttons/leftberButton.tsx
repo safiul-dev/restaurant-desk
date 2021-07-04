617570be0e7d40b85077f552a6c337df413ff557
Sabbir Ahmmed <sabbir@asoftwareltd.com>
2021-07-04 18:34:34 +0600

    doesn't show modal in leftberButton

--- renderer/components/Buttons/leftberButton.tsx
+++ renderer/components/Buttons/leftberButton.tsx
@@ -1,9 +1,27 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+
 export default function LeftberButton (props) {
+
+    const [modalOpen,setModalOpen] = useState(false)
+    const isClicked = ()=>{
+        modalOpen === true? setModalOpen(false):setModalOpen(true);
+        console.log(props.name,modalOpen)
+    };
+
+
+    Modal.setAppElement('#__next');
     return (
-            <button className="w-full bg-primary rounded h-leftberButtonHeight flex justify-center items-center mt-1">
+            <button type="button" className="w-full bg-primary rounded h-leftberButtonHeight flex justify-center items-center mt-1" onClick={isClicked}>
                     <h1 className="text-white uppercase not-italic sm:font-light md:font-light lg:font-semibold xl:font-bold tracking-wide font-sans sm:text-smallFont md:text-tiny lg:text-lg xl:text-xl 2xl:text-2xl sm:leading-3 md:leading-4 lg:leading-5 xl:leading-6 text-center">
                         {props.name}
                     </h1>
+                    <Modal
+                    isModel={modalOpen}
+                    style={{ overlay: 'gray', content: {}} }
+                    contentLabel="This is Modal"
+                    >
+                    </Modal>
             </button>
 
     )
--- renderer/components/leftber/leftMain.tsx
+++ renderer/components/leftber/leftMain.tsx
@@ -1,8 +1,48 @@
 import LeftberButton from "../Buttons/leftberButton";
 import LeftberMainButton from "../Buttons/leftberMainButton";
 
-export default function LeftMain () {
 
+
+export default function LeftMain () {
+ 
+    const arrObj = [
+        {
+        name:"change table",
+        showComponent:()=>{}
+    },
+    {
+        name:"select waiter",
+        showComponent:()=>{}
+    },
+    {
+        name:"select customer",
+        showComponent:()=>{}
+    },
+    {
+        name:"ticket note",
+        showComponent:()=>{}
+    },
+    {
+        name:"change table",
+        showComponent:()=>{}
+    },
+    {
+        name:"change table",
+        showComponent:()=>{}
+    },
+    {
+        name:"change table",
+        showComponent:()=>{}
+    },
+    {
+        name:"change table",
+        showComponent:()=>{}
+    },
+    {
+        name:"change table",
+        showComponent:()=>{}
+    },
+]
     const buttons = ['change table','select waiter', 'select customer', 'ticket note', 'canchel all', 'number of goust', 'print', 'split bill','add ticket']
     const listOfButton = buttons.map((name) =>< LeftberButton name={name} key={name} /> )
     return(
