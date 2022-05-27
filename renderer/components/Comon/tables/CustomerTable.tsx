be1f99466dbef7f6bf8871d7d8eaec5131f14ab2
ah-sabbir <ahsabbir103@gmail.com>
2022-05-27 17:49:50 +0600

    conflicts error can't solve it.

--- renderer/components/Comon/tables/CustomerTable.tsx
+++ renderer/components/Comon/tables/CustomerTable.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/display-name */
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   useFilters,
   useGlobalFilter,
@@ -7,72 +7,55 @@ import {
   useSortBy,
   useTable,
 } from "react-table";
-
+import { getDatas } from "../../../querys/getDatas";
 // import Swal from "sweetalert2";
 import LoaderAnimation from "../LoaderAnimation";
-import { getDatas } from "../../../querys/getDatas";
 
-const CustomerTable = ({isCreateModalOpen}) => {
+const CustomerTable = ({ isCreateModalOpen }) => {
   const [allUser, setAllUser] = useState([]) as any;
   const [pageNumber, setPageNumber] = useState(1);
   const [queryPageSize, setQueryPageSize] = useState(10);
   // start of table column related code
   const COLUMNS = [
-    
     {
       Header: "name",
       Cell: (row: any) => {
-        return (
-          <div className=" ">
-            {row?.cell?.row?.original?.name}
-          </div>
-        );
+        return <div className=" ">{row?.cell?.row?.original?.name}</div>;
       },
     },
     {
       Header: "email",
       Cell: (row: any) => {
-        return (
-          <div className=" ">
-            {
-              row?.cell?.row?.original?.email
-            }
-          </div>
-        );
+        return <div className=" ">{row?.cell?.row?.original?.email}</div>;
       },
     },
     {
       Header: "phone",
       Cell: (row: any) => {
-        return (
-          <div className=" ">
-            {row?.cell?.row?.original?.phone}
-          </div>
-        );
+        return <div className=" ">{row?.cell?.row?.original?.phone}</div>;
       },
     },
     {
       Header: "address",
       Cell: (row: any) => {
-        return (
-          <div className=" ">
-            {row?.cell?.row?.original?.address}
-          </div>
-        );
+        return <div className=" ">{row?.cell?.row?.original?.address}</div>;
       },
     },
-    
-   
+
     {
       Header: "Status",
       Cell: (row: any) => {
         return (
           <div className=" ">
-            {row?.cell?.row?.original?.status ? 
-            <span className=" px-2 py-0 bg-emerald-200 text-emerald-700 rounded-full">active</span> 
-            : 
-            <span className=" px-2 py-0 bg-red-200 text-red-700 rounded-full">deactive</span>
-            }
+            {row?.cell?.row?.original?.status ? (
+              <span className=" px-2 py-0 bg-emerald-200 text-emerald-700 rounded-full">
+                active
+              </span>
+            ) : (
+              <span className=" px-2 py-0 bg-red-200 text-red-700 rounded-full">
+                deactive
+              </span>
+            )}
           </div>
         );
       },
@@ -80,20 +63,18 @@ const CustomerTable = ({isCreateModalOpen}) => {
     {
       Header: "Action",
       Cell: (row: any) => {
-        
         return (
           <div className=" ">
-            <button 
-                onClick={() => editUser(row?.cell?.row?.original)} 
-                className="px-3 py-2 bg-sky-400 hover:bg-sky-500 transition duration-300 rounded text-white mr-2">
-                EDIT
+            <button
+              onClick={() => editUser(row?.cell?.row?.original)}
+              className="px-3 py-2 bg-sky-400 hover:bg-sky-500 transition duration-300 rounded text-white mr-2"
+            >
+              EDIT
             </button>
-            
           </div>
         );
       },
     },
-    
   ];
 
   const {
@@ -103,7 +84,7 @@ const CustomerTable = ({isCreateModalOpen}) => {
   } = getDatas({
     // page:       pageNumber,
     // pageSize:   queryPageSize,
-    url: '/customers'
+    url: "/customers",
   });
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => allUser, [allUser]);
@@ -130,7 +111,7 @@ const CustomerTable = ({isCreateModalOpen}) => {
       columns,
       data,
       initialState: {
-        pageIndex: pageNumber-1,
+        pageIndex: pageNumber - 1,
         pageSize: queryPageSize,
       },
       manualPagination: true,
@@ -139,7 +120,7 @@ const CustomerTable = ({isCreateModalOpen}) => {
     useFilters,
     useGlobalFilter,
     useSortBy,
-    usePagination,
+    usePagination
   );
 
   useEffect(() => {
@@ -155,7 +136,7 @@ const CustomerTable = ({isCreateModalOpen}) => {
     }
     gotoPage(0);
   }, [pageSize, gotoPage]);
-  
+
   useEffect(() => {
     dataRefetch();
     if (dataLists) {
@@ -163,29 +144,25 @@ const CustomerTable = ({isCreateModalOpen}) => {
     }
   }, [pageIndex, isCreateModalOpen]);
 
+  let [isEditUserModalOpen, setisEditUserModalOpen] = useState(false);
+  function closeEditUserModal() {
+    setisEditUserModalOpen(false);
+  }
+  function openEditUserModal() {
+    setisEditUserModalOpen(true);
+  }
 
+  // SEND USER DATA TO EDIT USER MODAL
 
-   let [isEditUserModalOpen, setisEditUserModalOpen] = useState(false)
-   function closeEditUserModal() {
-       setisEditUserModalOpen(false)
-   }
-   function openEditUserModal() {
-       setisEditUserModalOpen(true)
-   }
+  let [userToEdit, setUserToEdit] = useState({});
 
-   // SEND USER DATA TO EDIT USER MODAL
-
-   let [userToEdit, setUserToEdit] = useState({})
-
-   function editUser(user: any) {
-       setUserToEdit(user)
-       openEditUserModal()
-   }
-
-
+  function editUser(user: any) {
+    setUserToEdit(user);
+    openEditUserModal();
+  }
 
   function handlePageClick(e: any) {
-    setPageNumber(e.selected+1);
+    setPageNumber(e.selected + 1);
     gotoPage(e.selected);
   }
   function hendlePageSize(size: number) {
@@ -193,13 +170,13 @@ const CustomerTable = ({isCreateModalOpen}) => {
     setPageSize(size);
   }
 
-  function hendleNextPage () {
-    nextPage()
-    setPageNumber(pageIndex+2);
+  function hendleNextPage() {
+    nextPage();
+    setPageNumber(pageIndex + 2);
   }
 
   function handlePrevPage() {
-    previousPage()
+    previousPage();
     setPageNumber(pageIndex);
   }
   if (isLoading) {
@@ -207,7 +184,6 @@ const CustomerTable = ({isCreateModalOpen}) => {
   }
   return (
     <div>
-     
       {/* <EditUserModal
         isOpen={isEditUserModalOpen} 
         closeModal={closeEditUserModal} 
@@ -215,78 +191,79 @@ const CustomerTable = ({isCreateModalOpen}) => {
         allUser={allUser} 
         setAllUser={setAllUser} 
       /> */}
-        <table
-          className="bg-secondary w-width99% mx-auto my-1 overflow-y-auto"
-          {...getTableProps()}
-        >
-          <thead className="bg-gray text-white uppercase">
-            {headerGroups.map((headerGroup: any, idx: any) => (
-              <tr className="border-white border"
-                 key={idx} {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column: any, idx: any) => (
-                  <th
+      <table
+        className="bg-secondary w-width99% mx-auto my-1 overflow-y-auto"
+        {...getTableProps()}
+      >
+        <thead className="bg-gray text-white uppercase">
+          {headerGroups.map((headerGroup: any, idx: any) => (
+            <tr
+              className="border-white border"
+              key={idx}
+              {...headerGroup.getHeaderGroupProps()}
+            >
+              {headerGroup.headers.map((column: any, idx: any) => (
+                <th
+                  key={idx}
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  scope="col"
+                  className="border-white border text-center"
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row: any, idx: any) => {
+            prepareRow(row);
+
+            return (
+              <tr
+                key={idx}
+                {...row.getRowProps()}
+                className="odd:bg-white even:bg-gray-50"
+              >
+                {row.cells.map((cell: any, idx: any) => (
+                  <td
                     key={idx}
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
                     scope="col"
                     className="border-white border text-center"
                   >
-                    {column.render("Header")}
-                  </th>
+                    {cell.render("Cell")}
+                  </td>
                 ))}
               </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row: any, idx: any) => {
-              prepareRow(row);
-              
-              return (
-                <tr
-                  key={idx}
-                  {...row.getRowProps()}
-                  className="odd:bg-white even:bg-gray-50"
-                >
-                  {row.cells.map((cell: any, idx: any) => (
-                    <td
-                      key={idx}
-                      scope="col"
-                      className="border-white border text-center"
-                    >
-                      {cell.render("Cell")}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+            );
+          })}
+        </tbody>
+      </table>
 
-        <div className="flex">
-          <div className="flex-1">
-            <span className="mr-4 text-black-tint-200">
-              Page{" "}
-              <strong className="text-steelblue-tint-100">
-                {pageIndex+1} of {pageCount}
-              </strong>
-            </span>
-           
-            <span>
-               Page Size:{" "}
-              <select
-                value={pageSize}
-                onChange={(e) => hendlePageSize(Number(e.target.value))}
-              >
-                {[10, 20, 30, 40, 50, 100].map((pageSize: any, i: number) => (
-                  <option key={i} value={pageSize}>
-                    {pageSize}
-                  </option>
-                ))}
-              </select>
-            </span>
-          
-          </div>
-       
+      <div className="flex">
+        <div className="flex-1">
+          <span className="mr-4 text-black-tint-200">
+            Page{" "}
+            <strong className="text-steelblue-tint-100">
+              {pageIndex + 1} of {pageCount}
+            </strong>
+          </span>
+
+          <span>
+            Page Size:{" "}
+            <select
+              value={pageSize}
+              onChange={(e) => hendlePageSize(Number(e.target.value))}
+            >
+              {[10, 20, 30, 40, 50, 100].map((pageSize: any, i: number) => (
+                <option key={i} value={pageSize}>
+                  {pageSize}
+                </option>
+              ))}
+            </select>
+          </span>
         </div>
+      </div>
     </div>
   );
 };
